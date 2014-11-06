29b5eea8f857cf13f1773cada6f7bc1a1470e0c6
Ken Faulkner <Ken.Faulkner@rockend.com.au>
2014-11-07 05:20:41 +1100

    Revert "Removed Fake tree node."
    This reverts commit d5055316f08f7a3e94e08c547c50535226d21d94.

--- DocumentDBSudio/TreeNodes.cs
+++ DocumentDBSudio/TreeNodes.cs
@@ -777,6 +777,8 @@ namespace Microsoft.Azure.DocumentDBStudio
             }
             else if (this.resourceType == ResourceType.Document)
             {
+                this.Nodes.Add(new TreeNode("Fake"));
+
                 this.contextMenu.MenuItems.Add("-");
 
                 MenuItem myMenuItem3 = new MenuItem("Add attachment");
